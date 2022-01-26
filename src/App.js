7736ea1455e9616614430a27fe1856ec97bd61d3
Independent <38752974+surachokh@users.noreply.github.com>
2022-01-26 20:40:01 +0700

    Add: Fetch Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import { Card, CardContent, createTheme, ThemeProvider } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  createTheme,
+  List,
+  ThemeProvider,
+} from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
-import Content from "./components/Content"
-import Footer from "./components/Footer"
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import axios from "axios";
 
 export default function App() {
   const theme = createTheme({
@@ -11,6 +18,20 @@ export default function App() {
       fontFamily: ['"Prompt"', "sans-serif"].join(","),
     },
   });
+
+  const [data, setData] = useState({ major: [] });
+
+  useEffect(() => {
+    axios
+      .get("https://tcas-assets.skooldio.com/tmp/mock_tcaster_api.json")
+      .then((res) => {
+        const result = res.data;
+        setData({
+          major: result,
+        });
+      });
+  }, []);
+
   return (
     <div>
       <ThemeProvider theme={theme}>
@@ -23,15 +44,19 @@ export default function App() {
             height: "100vh",
           }}
         >
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Header />
-              <hr />
-              <Content />
-              <hr />
-              <Footer />
-            </CardContent>
-          </Card>
+          <List sx={{display: 'flex', flexDirection: 'row', justifyContent: "space-around", flexGrow: 1}}>
+            {data.major.map((major) => (
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Header />
+                  <hr />
+                  <Content />
+                  <hr />
+                  <Footer />
+                </CardContent>
+              </Card>
+            ))}
+          </List>          
         </Box>
       </ThemeProvider>
     </div>
